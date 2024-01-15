95ff72820f2a72c6d4643d1a72ff9b5c92f8c8eb
Alan <alanbling0120@gmail.com>
2024-01-15 14:07:52 +0800

    Just commit ignore content

---
+++ General-Server-1118/src/main/java/com/alandevise/handler/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.alandevise.handler;
+
+import com.alandevise.exception.BusinessException;
+import com.alibaba.nacos.api.naming.pojo.healthcheck.impl.Http;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @Filename GlobalExceptionHandler1.java
+ * @Package com.alandevise.handler
+ * @Version V1.0.0
+ * @Description 1.
+ * @Author Alan Zhang [initiator@alandevise.com]
+ * @Date 2023-12-04 17:45
+ */
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler{
+    @ExceptionHandler({BusinessException.class})
+    @ResponseStatus(HttpStatus.OK)
+    public String BusinessExceptionReply(BusinessException businessException){
+        return businessException.getCode()+"\n"+businessException.getMessage();
+    }
+}
