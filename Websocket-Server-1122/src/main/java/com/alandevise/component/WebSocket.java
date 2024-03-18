f57bcfc43c0f6a2b0486f080438d4affe8a05c6d
Alan Zhang <initiator@alandevise.com>
2024-03-18 23:15:06 +0800

    添加更多注释内容

--- Websocket-Server-1122/src/main/java/com/alandevise/component/WebSocket.java
+++ Websocket-Server-1122/src/main/java/com/alandevise/component/WebSocket.java
@@ -20,7 +20,10 @@ import java.util.concurrent.ConcurrentHashMap;
  * @Date: 2024年03月18日 22:04
  */
 
+
 @ServerEndpoint("/websocket/{pageCode}")
+//  @ServerEndpoint 注解是一个类层次的注解，它的功能主要是将目前的类定义成一个websocket服务器端,注解的值将被用于监听用户连接的终端访问URL地址,
+//  客户端可以通过这个URL来连接到WebSocket服务器端
 @Component
 public class WebSocket {
 
